3a1f39122449fcf2bc0cc2587a778fcfa721a9fa
Erik Youngren <artanis.00@gmail.com>
2019-08-23 17:41:22 -0700

    Update `Turn` tests to expect a player object.

--- src/gamestate.test.js
+++ src/gamestate.test.js
@@ -1,25 +1,27 @@
 'use strict';
 
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+import {Player} from "./player";
+
+/**
+ * Testing
+ */
 import {Turn} from "./gamestate";
 
-// import {Player} from "./player";
-// jest.mock('player');
 
 describe("Turn object", () => {
-  var turn;
-
-  beforeEach(() => {
-    turn = new Turn(0, 0);
-  });
-
   test("Follows spec", () => {
+    var player = Player.X();
+    var turn = new Turn(player, 0, 0);
+
     expect(turn).toEqual(expect.objectContaining({
       id: expect.stringMatching(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i),
-      // player: expect.toBeInstanceOf(Player),
+      player: expect.any(Player),
       square: expect.any(Number),
-      subsquare: expect.any(Number),
-      gameWinning: undefined,
-      subgameWinning: undefined
+      subsquare: expect.any(Number)
     }));
   });
 });
